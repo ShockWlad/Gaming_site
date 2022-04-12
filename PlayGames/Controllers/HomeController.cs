675c8a355fd33e5a545caac7c0776c2d9eaf6ccc
ShockWlad <porada.vd1000@gmail.com>
2022-04-12 20:49:50 +0300

    God job

---
+++ PlayGames/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayGames.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Play1()
+		{
+			return View();
+		}
+		public IActionResult Play2()
+		{
+			return View();
+		}
+
+		public IActionResult Reg()
+		{
+			return View();
+		}
+	}
+}
